62189613a3bfd27dfe2bd6e41462cceeeb071b87
Sachin Trivedi <sachindtrivedi@gmail.com>
2020-04-12 11:58:51 +0530

    Updates

---
+++ precache-manifest.899d3120ce880c2f0f3d865fb203ffcf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "690dd94b6790e960cc590a3562db7928",
+    "url": "/react-user-search/index.html"
+  },
+  {
+    "revision": "b738f8c0a58956e0debe",
+    "url": "/react-user-search/static/css/main.0b846b8f.chunk.css"
+  },
+  {
+    "revision": "448a95dd7575a38ee9c4",
+    "url": "/react-user-search/static/js/2.c5d6359e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-user-search/static/js/2.c5d6359e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b738f8c0a58956e0debe",
+    "url": "/react-user-search/static/js/main.44285418.chunk.js"
+  },
+  {
+    "revision": "9447347019504f61ed1d",
+    "url": "/react-user-search/static/js/runtime-main.f2f8efa0.js"
+  }
+]);
